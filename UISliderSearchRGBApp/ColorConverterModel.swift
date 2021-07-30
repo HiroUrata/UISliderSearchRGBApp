5a93d1fbdb6a36d18f1c782711bfcd7310cbff7a
HiroUrata <1712026@i.softbank.jp>
2021-07-30 18:19:35 +0900

    UISliderでcolorDebugViewの背景色を変える　追加

--- UISliderSearchRGBApp/ColorConverterModel.swift
+++ UISliderSearchRGBApp/ColorConverterModel.swift
@@ -12,7 +12,7 @@ class ColorConvertModel{
     
     func colorConvert(redValue:CGFloat,greenValue:CGFloat,blueValue:CGFloat,targetView:UIView){
         
-        
+        targetView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
         
     }
     
--- UISliderSearchRGBApp/ViewController.swift
+++ UISliderSearchRGBApp/ViewController.swift
@@ -13,22 +13,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
     
+    let colorConverterModel = ColorConvertModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        colorDebugView.backgroundColor =  UIColor.black
+        
+        redSlider.value = 0.00
+        redSlider.minimumValue = 0.00
+        redSlider.maximumValue = 1.00
+        
+        greenSlider.value = 0.00
+        greenSlider.minimumValue = 0.00
+        greenSlider.maximumValue = 1.00
+        
+        blueSlider.value = 0.00
+        blueSlider.minimumValue = 0.00
+        blueSlider.maximumValue = 1.00
+        
+    }
 
     @IBAction func redSlide(_ sender: UISlider) {
+        
+        colorConverterModel.colorConvert(redValue: CGFloat(sender.value), greenValue: CGFloat(greenSlider.value), blueValue: CGFloat(blueSlider.value), targetView: colorDebugView)
+        
+        redValueLabel.text = String(floor(sender.value * 100) / 100)
         
     }
     
     @IBAction func greenSlide(_ sender: UISlider) {
         
+        colorConverterModel.colorConvert(redValue: CGFloat(redSlider.value), greenValue: CGFloat(sender.value), blueValue: CGFloat(blueSlider.value), targetView: colorDebugView)
+        
+        greenValueLabel.text =  String(floor(sender.value * 100) / 100)
+        
     }
     
     @IBAction func blueSlide(_ sender: UISlider) {
+        
+        colorConverterModel.colorConvert(redValue: CGFloat(redSlider.value), greenValue: CGFloat(greenSlider.value), blueValue: CGFloat(sender.value), targetView: colorDebugView)
+     
+        blueValueLabel.text =  String(floor(sender.value * 100) / 100)
         
     }
     
