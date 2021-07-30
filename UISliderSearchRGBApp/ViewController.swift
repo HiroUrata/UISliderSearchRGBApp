5f0a78c9a34be437c1412d4dd627b005979fcaf1
HiroUrata <1712026@i.softbank.jp>
2021-07-30 16:35:35 +0900

    Initial Commit

---
+++ UISliderSearchRGBApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISliderSearchRGBApp
+//
+//  Created by UrataHiroki on 2021/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
